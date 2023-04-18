6f40c9bcc415c0eadb6f912391a3785ff47d006c
Johannes Zottele <private@johannes-zottele.at>
2023-04-18 13:20:29 +0200

    add unijit-exec argument to cacao configuration

--- tools/configure_cacao.sh
+++ tools/configure_cacao.sh
@@ -6,6 +6,7 @@ echo "Configure Cacao into $DOCK_BUILD_DIR"
 cd $DOCK_BUILD_DIR
 $DOCK_SRC_DIR/configure $@ --enable-unijit --enable-disassembler --enable-debug --enable-logging --enable-maintainer-mode \
       --with-unijit-src=/code/jtl \
+      --with-unijit-exec=/code/jtl/build/unijit \
       --with-java-runtime-library=gnuclasspath \
       --with-java-runtime-library-prefix=/usr/local/classpath \
       --with-junit-jar=/usr/share/java/junit4.jar:/usr/share/java/hamcrest.jar
